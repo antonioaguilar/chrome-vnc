dab5a112b8edbf73da8d2f87f946ff44664d31ff
Antonio Aguilar <ab6446694@gmail.com>
2018-04-12 15:19:54 +0100

    updated script.sh config

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 export DISPLAY=":1"
-vncserver $DISPLAY -geometry 1440x900 && tail -f /root/.vnc/*.log & protractor $@
+
+if [ -z "$PORT" ]
+then
+  VNC_PORT=5901
+else
+  VNC_PORT=$PORT
+fi
+
+vncserver $DISPLAY -geometry 1280x800 -rfbport $VNC_PORT && tail -f /root/.vnc/*.log & protractor $@
